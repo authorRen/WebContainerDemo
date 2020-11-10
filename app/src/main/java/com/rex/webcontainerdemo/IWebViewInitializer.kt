6534cae33657e76ed7b53bd12c01578bb81e6dec
renzeqiang07329 <renzeqiang07329@hellobike.com>
2020-11-10 15:02:58 +0800

    first commit

---
+++ app/src/main/java/com/rex/webcontainerdemo/IWebViewInitializer.kt
@@ -0,0 +1,8 @@
+package com.rex.webcontainerdemo
+
+/**
+ * Created by rzq on 2020/11/10
+ */
+interface IWebViewInitializer {
+
+}
---
+++ app/src/main/java/com/rex/webcontainerdemo/WebDelegate.kt
@@ -0,0 +1,10 @@
+package com.rex.webcontainerdemo
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by rzq on 2020/11/10
+ */
+class WebDelegate : Fragment() {
+
+}
